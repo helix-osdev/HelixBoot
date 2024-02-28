f92a742c3d43d3b214b9047c9f7cfb319f2e9f63
PC-Doctor666 <austingarrick752@gmail.com>
2024-02-28 04:32:28 +0000

    ELF: fix typo

--- include/elf/elf_api.h
+++ include/elf/elf_api.h
@@ -13,7 +13,7 @@ elf_fd_t *elf_open(char16_t *name);
 void elf_close(elf_fd_t *fd);
 efi_status_t elf_load(elf_fd_t *fd);
 
-bool elf_validate(elf_fd_t *fd)
+bool elf_validate(elf_fd_t *fd);
 bool elf_is_supported_arch(elf_fd_t *fd);
 
 #endif
