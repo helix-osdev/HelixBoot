c2d5a93339f0004c6c631f8cecbff12df1718c58
PC-Doctor666 <austingarrick752@gmail.com>
2024-03-01 06:10:16 +0000

    ALLOC: handle page allocation in boot services

--- lib/efi/alloc.c
+++ lib/efi/alloc.c
@@ -4,21 +4,52 @@
 
 
 
-void *malloc(uint64_t length) {
+void *alloc(uint64_t length) {
 	efi_status_t ret = 0;
 	void *buf = NULL;
 
-	if (length <= 0) {
-		return NULL;
-	}
 
 	ret = BS->allocate_pool(EfiLoaderData,
 			length,
 			(void *)&buf);
 
-	if (EFI_ERROR(ret) && !buf) {
+	if (EFI_ERROR(ret)) {
 		return NULL;
 	}
 
 	return buf;
+}
+
+void *alloc_page(void) {
+	efi_status_t ret = 0;
+	void *buf = NULL;
+
+
+	ret = BS->allocate_pages(AllocateAnyPages,
+			EfiLoaderData,
+			1,
+			(void *)&buf);
+
+	if (EFI_ERROR(ret)) {
+		return NULL;
+	}
+
+	return buf:
+}
+
+void *alloc_pages(uint64_t length) {
+	efi_status_t ret = 0;
+	void *buf = NULL;
+
+
+	ret = BS->allocate_pages(AllocateAnyPages,
+			EfiLoaderData,
+			EFI_SIZE_TO_PAGES(length),
+			(void *)&buf);
+
+	if (EFI_ERROR(ret)) {
+		return NULL;
+	}
+
+	return buf:
 }
