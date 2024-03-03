fd291aefbe00c184f7a503f359f725f898a3c9bc
PC-Doctor666 <austingarrick752@gmail.com>
2024-03-03 18:32:38 +0000

    ELF: start section header parsing

--- include/elf/elf_api.h
+++ include/elf/elf_api.h
@@ -10,8 +10,12 @@
 
 elf_fd_t *elf_open(char16_t *name);
 void elf_close(elf_fd_t *fd);
-efi_status_t elf_check(void *buf);
+efi_status_t elf_check(void *elf_data);
 
-uint64_t elf_shdr_size(void *buf);
+uint64_t elf_shdr_size(elf_fd_t *fd);
+efi_status_t elf_load(elf_fd_t *fd);
+
+elf64_shdr_t *elf_get_shdr(elf_fd_t *fd);
+elf64_shdr_t *elf_for_each_shdr(elf_fd_t *fd, uint64_t idx);
 
 #endif
