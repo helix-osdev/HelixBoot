0c6f0078f0024be982bf1ded2d7c44e84584c547
PC-Doctor666 <austingarrick752@gmail.com>
2024-03-04 18:29:07 +0000

    ELF: fix missing version field + various fixes

--- include/elf/elf_def.h
+++ include/elf/elf_def.h
@@ -4,6 +4,22 @@
 #include <efi.h>
 
 
+typedef uint16_t elf64_half_t;
+
+typedef uint32_t elf64_word_t;
+typedef int32_t elf64_sword_t;
+typedef uint64_t elf64_xword_t;
+typedef int64_t elf64_sxword_t;
+
+typedef uint64_t elf64_addr_t;
+typedef uint64_t elf64_off_t;
+
+typedef uint16_t elf64_section_t;
+
+typedef elf64_half_t elf64_versym_t;
+
+
+
 #define MAX_ELF_IDENT 16
 
 #define EI_MAG0		0
@@ -44,6 +60,7 @@
 
 #define EV_NONE		0
 #define EV_CURRENT	1
+#define EV_NUM		2
 
 #define PF_R	0x4
 #define PF_W	0x2
@@ -87,59 +104,88 @@
 
 #define R_AARCH64_RELATIVE 0x00000403
 
+#define ELF64_ST_BIND(x)   	((x) >> 4)
+#define ELF64_ST_TYPE(x)   	((x) & 0xf)
+#define ELF64_ST_INFO(x, y) (((x) << 4) + ((y) & 0xf))
+
+#define ELF32_ST_VISIBILITY(x) ((x) & 0x3)
+#define ELF64_ST_VISIBILITY(x) ((x) & 0x3)
+
+
+
+
 
 
 typedef struct {
-	uint8_t		e_ident[MAX_ELF_IDENT];
-	uint16_t	e_type;
-	uint16_t	e_machine;
-	uint64_t	e_entry;
-	uint64_t	e_phoff;
-	uint64_t	e_shoff;
-	uint64_t	e_flags;
-	uint16_t	e_ehsize;
-	uint16_t	e_phentsize;
-	uint16_t	e_phnum;
-	uint16_t	e_shentsize;
-	uint16_t	e_shnum;
-	uint16_t	e_shstrndx;
+	uint8_t			e_ident[MAX_ELF_IDENT];
+	elf64_half_t	e_type;
+	elf64_half_t	e_machine;
+	elf64_word_t	e_version;
+	elf64_addr_t	e_entry;
+	elf64_off_t		e_phoff;
+	elf64_off_t		e_shoff;
+	elf64_word_t	e_flags;
+	elf64_half_t	e_ehsize;
+	elf64_half_t	e_phentsize;
+	elf64_half_t	e_phnum;
+	elf64_half_t	e_shentsize;
+	elf64_half_t	e_shnum;
+	elf64_half_t	e_shstrndx;
 } elf64_ehdr_t;
 
 typedef struct {
-	uint32_t	p_type;
-	uint32_t	p_flags;
-	uint64_t	p_offset;
-	uint64_t	p_vaddr;
-	uint64_t	p_paddr;
-	uint64_t	p_filesz;
-	uint64_t	p_memsz;
-	uint64_t	p_align;
+	elf64_word_t	p_type;
+	elf64_word_t	p_flags;
+	elf64_off_t		p_offset;
+	elf64_addr_t	p_vaddr;
+	elf64_addr_t	p_paddr;
+	elf64_xword_t	p_filesz;
+	elf64_xword_t	p_memsz;
+	elf64_xword_t	p_align;
 } elf64_phdr_t;
 
 typedef struct {
-    uint32_t sh_name;
-    uint32_t sh_type;
-    uint64_t sh_flags;
-    uint64_t sh_addr;
-    uint64_t sh_offset;
-    uint64_t sh_size;
-    uint32_t sh_link;
-    uint32_t sh_info;
-    uint64_t sh_addralign;
-    uint64_t sh_entsize;
+    elf64_word_t 	sh_name;
+    elf64_word_t	sh_type;
+    elf64_xword_t	sh_flags;
+    elf64_addr_t	sh_addr;
+    elf64_off_t		sh_offset;;
+    elf64_xword_t	sh_size;
+    elf64_word_t	sh_link;
+    elf64_word_t	sh_info;
+    elf64_xword_t 	sh_addralign;
+    elf64_xword_t	sh_entsize;
 } elf64_shdr_t;
 
 typedef struct {
-    uint64_t r_addr;
-    uint32_t r_info;
-    uint32_t r_symbol;
-    uint64_t r_addend;
+	elf64_addr_t	r_offset;
+	elf64_xword_t	r_info;
+} elf64_rel_t;
+
+typedef struct {
+    elf64_addr_t	r_offset;
+    elf64_xword_t	r_info;
+    elf64_sxword_t	r_addend;
 } elf64_rela_t;
 
 typedef struct {
     uint64_t d_tag;
     uint64_t d_un;
 } elf_dyn_t;
+
+typedef struct {
+	elf64_word_t	st_name;
+	uint8_t			st_info;
+	uint8_t			st_other;
+	elf64_section_t	st_shndx;
+	elf64_addr_t	st_value;
+	elf64_xword_t	st_size;
+} elf64_sym_t;
+
+typedef struct {
+	elf64_half_t	si_bound_to;
+	elf64_half_t	si_flags;
+} elf64_sym_info_t;
 
 typedef struct {
 	efi_file_proto_t	*elf_fd;
