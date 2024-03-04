abddfa3b29fd0922832bc2642aeb849c742ab646
PC-Doctor666 <austingarrick752@gmail.com>
2024-03-04 21:59:26 +0000

    Test parsing section headers + allocate SHT_NOBITS sections

--- lib/elf/elf_load.c
+++ lib/elf/elf_load.c
@@ -1,5 +1,6 @@
 #include <efi.h>
 #include <elf/elf.h>
+#include <devices/disk.h>
 
 
 
@@ -9,7 +10,8 @@
 efi_status_t elf_load(elf_fd_t *fd) {
 	efi_status_t ret = 0;
 	elf64_ehdr_t *hdr = (elf64_ehdr_t *)fd->elf_hdr;
-	elf64_phdr_t *phdr = NULL;
+	elf64_shdr_t *shdr = NULL;
+
 
 
 	ret = elf_check(fd);
@@ -19,13 +21,36 @@ efi_status_t elf_load(elf_fd_t *fd) {
 		return ret;
 	}
 
-	for (uint64_t i = 0; i < hdr->e_phnum; i++) {
-		phdr = elf_for_each_phdr(fd, i);
+	for (uint64_t i = 0; i < hdr->e_shnum; i++) {
+		shdr = elf_for_each_shdr(fd, i);
 
-		printf(L"Size: %u\n", phdr->p_filesz);
-		printf(L"Phys: %x\n", phdr->p_paddr);
+		if (shdr->sh_type == SHT_NULL) {
+			// NULL segment
+			continue;
+		}
 
-		free(phdr);
+		if (shdr->sh_type == SHT_NOBITS) {
+			// Section needs allocation
+
+			if (shdr->sh_size <= 0) {
+				// Empty section
+				continue;
+			}
+
+			if (shdr->sh_flags & SHF_ALLOC) {
+				// Allocate section
+				void *buf = alloc(shdr->sh_size);
+				memset(buf, 0, shdr->sh_size);
+
+				elf64_off_t hdr_off = (elf64_off_t)hdr;
+				elf64_off_t buf_off = (elf64_off_t)buf;
+
+				// Set offset to allocated buffer
+				shdr->sh_offset = hdr_off - buff_off;
+			}
+		}
+
+		free(shdr);
 	}
 
 	return EFI_SUCCESS;
