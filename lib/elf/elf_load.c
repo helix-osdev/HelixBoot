65cb7e03bfa7df652a58e3c38874fa99761d369b
PC-Doctor666 <austingarrick752@gmail.com>
2024-03-05 03:03:26 +0000

    ELF: don't free section headers (memory map them)

--- lib/elf/elf_load.c
+++ lib/elf/elf_load.c
@@ -7,6 +7,8 @@
 
 
 
+
+
 efi_status_t elf_load(elf_fd_t *fd) {
 	efi_status_t ret = 0;
 	elf64_ehdr_t *hdr = (elf64_ehdr_t *)fd->elf_hdr;
@@ -24,33 +26,30 @@ efi_status_t elf_load(elf_fd_t *fd) {
 	for (uint64_t i = 0; i < hdr->e_shnum; i++) {
 		shdr = elf_for_each_shdr(fd, i);
 
-		if (shdr->sh_type == SHT_NULL) {
-			// NULL segment
-			continue;
+		switch(shdr->sh_type) {
+			case SHT_NOBITS:
+				if (shdr->sh_size <= 0) {
+					// NULL segment
+					continue;
+				}
+
+				if (shdr->sh_flags & SHF_ALLOC) {
+					// Allocate section
+					void *buf = alloc(shdr->sh_size);
+					memset(buf, 0, shdr->sh_size);
+
+					elf64_off_t hdr_off = (elf64_off_t)hdr;
+					elf64_off_t buf_off = (elf64_off_t)buf;
+
+					// Set offset to allocated buffer
+					shdr->sh_offset = hdr_off - buf_off;
+				}
+
+				break;
+
+			default:
+				break;
 		}
-
-		if (shdr->sh_type == SHT_NOBITS) {
-			// Section needs allocation
-
-			if (shdr->sh_size <= 0) {
-				// Empty section
-				continue;
-			}
-
-			if (shdr->sh_flags & SHF_ALLOC) {
-				// Allocate section
-				void *buf = alloc(shdr->sh_size);
-				memset(buf, 0, shdr->sh_size);
-
-				elf64_off_t hdr_off = (elf64_off_t)hdr;
-				elf64_off_t buf_off = (elf64_off_t)buf;
-
-				// Set offset to allocated buffer
-				shdr->sh_offset = hdr_off - buff_off;
-			}
-		}
-
-		free(shdr);
 	}
 
 	return EFI_SUCCESS;
