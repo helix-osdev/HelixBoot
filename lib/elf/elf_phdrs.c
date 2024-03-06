30e4e39db1b4e4de3005e9ffcd853e110303e670
PC-Doctor666 <austingarrick752@gmail.com>
2024-03-06 01:17:03 +0000

    ELF: load program header to it's physical address directly (no address allocations)

--- lib/elf/elf_phdrs.c
+++ lib/elf/elf_phdrs.c
@@ -32,6 +32,9 @@ elf64_phdr_t *elf_for_each_phdr(elf_fd_t *fd, uint64_t index) {
 efi_status_t elf_load_phdr(elf_fd_t *fd, elf64_phdr_t *phdr) {
 	efi_status_t ret = 0;
 	elf64_addr_t phys_base = 0, virt_base = 0;
+	elf64_addr_t *segment;
+	elf64_xword_t file_size = 0, segment_size = 0;
+	elf64_xword_t segment_pages = 0;
 	elf64_ehdr_t *hdr = (elf64_ehdr_t *)fd->elf_hdr;
 
 
@@ -40,25 +43,21 @@ efi_status_t elf_load_phdr(elf_fd_t *fd, elf64_phdr_t *phdr) {
 		case PT_LOAD:
 			phys_base = phdr->p_paddr;
 			virt_base = phdr->p_vaddr;
-			
-			elf64_addr_t segment = phys_base;
-			elf64_xword_t segment_size = phdr->p_filesz;
-			elf64_xword_t segment_pages = EFI_SIZE_TO_PAGES(segment_size);
 
 			if (phys_base <= 0x0 || virt_base <= 0x0) {
 				// NULL segment
 				return EFI_NOT_FOUND;
 			}
 
-			// Load segment to it's physical address
-			elf_alloc_addr(segment, segment_pages);
+			file_size = phdr->p_filesz;
+			segment_size = phdr->p_memsz;
+			segment_pages = EFI_SIZE_TO_PAGES(segment_size);
+			segment = (elf64_addr_t *)phys_base;
 
-			if (!segment) {
-				printf(L"elf: failed to load segment %x\n", segment);
-				return EFI_LOAD_ERROR;
-			}
-
-			printf(L"Segment addr: %x\n", segment);
+			// Read program header into it's physical
+			// address space
+			fseek(fd->elf_fd, phdr->p_offset);
+			fread(fd->elf_fd, (elf64_addr_t *)segment, phdr->p_filesz);
 
 			break;
 	}
