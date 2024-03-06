9f91f7cc69a5584ceda8633a89143cd9159df9d1
PC-Doctor666 <austingarrick752@gmail.com>
2024-03-06 05:41:58 +0000

    Load phdrs + shdrs

--- lib/elf/elf_load.c
+++ lib/elf/elf_load.c
@@ -30,15 +30,21 @@ efi_status_t elf_load(elf_fd_t *fd) {
 			for (uint64_t i = 0; i < hdr->e_phnum; i++) {
 				phdr = elf_for_each_phdr(fd, i);
 
-				// TODO:
-				// Check section's sizes and ensure that
-				// if they end up in the same region and
-				// ensure that we allocate each address
-				// for each one
 				ret = elf_load_phdr(fd, phdr);
 
 				if (EFI_ERROR(ret)) {
-					printf(L"elf: failed to load segment @ index %u\n", i);
+					printf(L"elf: failed to load phdr segment @ index %u\n", i);
+					return ret;
+				}
+			}
+
+			for (uint64_t i = 0; i < hdr->e_shnum; i++) {
+				shdr = elf_for_each_shdr(fd, i);
+
+				ret = elf_load_shdr(fd, shdr);
+
+				if (EFI_ERROR(ret)) {
+					printf(L"elf: failed to load shdr segment @ index %u\n", i);
 					return ret;
 				}
 			}
@@ -56,46 +62,6 @@ efi_status_t elf_load(elf_fd_t *fd) {
 		default:
 			break;
 	}
-
-
-	/*
-	for (uint64_t i = 0; i < hdr->e_shnum; i++) {
-		shdr = elf_for_each_shdr(fd, i);
-
-		switch(shdr->sh_type) {
-			case SHT_NOBITS:
-				if (shdr->sh_size <= 0) {
-					// NULL segment
-					continue;
-				}
-
-				switch(shdr->sh_flags) {
-					case SHF_ALLOC:
-						// Allocate section
-						void *buf = elf_alloc(shdr->sh_size);
-						memset(buf, 0, shdr->sh_size);
-
-						elf64_off_t hdr_off = (elf64_off_t)hdr;
-						elf64_off_t buf_off = (elf64_off_t)buf;
-
-						// Set offset to allocated buffer
-						shdr->sh_offset = hdr_off - buf_off;
-					case SHF_WRITE:
-						// TODO:
-						// Map as writable
-						break;
-
-					default:
-						break;
-				}
-
-				break;
-
-			default:
-				break;
-		}
-	}
-	*/
 
 	return EFI_SUCCESS;
 }
