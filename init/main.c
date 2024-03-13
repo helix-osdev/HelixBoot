cf5c5ff90b20e6263a27b28d8ba3e2cc2805ac45
helix-osdev <austingarrick752@gmail.com>
2024-03-13 20:37:12 +0000

    Remove unneeded include

--- init/main.c
+++ init/main.c
@@ -1,6 +1,5 @@
 #include <efi.h>
 #include <mm/mm.h>
-#include <mm/paging.h>
 #include <elf/elf.h>
 #include <arch/asm.h>
 #include <bootinfo.h>
@@ -45,57 +44,6 @@ efi_status_t helix_main(efi_handle_t img_handle, efi_system_table_t *sys_tbl) {
 	// Populate boot info data for kernel use. Do this
 	// here to ensure we exit boot services without issue
 	boot_info.mm = &m;
-
-	// Things from this point forward will be a bit
-	// quick and dirty just to get the kernel into
-	// virtual memory. I still can't get QEMU to play
-	// nice with any paging setup so this *should* work
-	// for the mean time
-
-	printf(L"Updating page tables...\n");
-	paging_init(&m);
-
-	mmu_disable();
-	isb();
-
-	set_mair(0);
-	set_tcr(0);
-	set_ttbr0(0);
-	set_ttbr1(0);
-	dsb(sy);
-	isb();
-
-	set_mair(MAIR_VALUE);
-
-	uint64_t tcr = read_tcr();
-	tcr |= TCR_T0SZ(39);
-	tcr |= TCR_IRGN0_WBWA;
-	tcr |= TCR_ORGNO_WBWA;
-	tcr |= TCR_SH0_ISH;
-	tcr |= TCR_TG0_4K;
-	tcr |= TCR_IPS_4G;
-
-	tcr |= TCR_T1SZ(39);
-	tcr |= TCR_IRGN1_WBWA;
-	tcr |= TCR_ORGN1_WBWA;
-	tcr |= TCR_SH1_ISH;
-	tcr |= TCR_TG1_4K;
-	tcr |= TCR_EPD1_ENABLE;
-	set_tcr(tcr);
-	isb();
-
-	tlbi(vmalle1);
-	dsb(sy);
-	isb();
-
-	update_page_tables();
-	mmu_enable();
-	isb();
-
-	printf(L"Done!\n");
-
-	// We're ready pass control to the kernel
-	elf_exec(fd, &boot_info);
 
 	while(1);
 }
